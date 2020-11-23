4b3159a169de387675e34974b5ae37da33877af3
yangjunqiang <yangjunqiang1992@foxmail.com>
2020-11-23 21:13:45 +0800

    提交redis多db实例代码

---
+++ spring-boot-redis-multi-db-demo/src/test/java/com/smile/demo/multidb/SpringBootRedisMultiDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smile.demo.multidb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRedisMultiDbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
